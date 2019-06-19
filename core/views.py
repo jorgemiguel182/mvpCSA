31a5bd2ddce76cdad85bec933d8d61242f2ce53c
Jorge <jmdinfo123@gmail.com>
2019-06-19 19:21:03 -0300

    arrumando urls2

--- core/views.py
+++ core/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'core/home.html', {})
+
+    return render(request, 'core/home.html')
+
+
+def profissionais(request):
+    return render(request, 'core/profissionais.html')
