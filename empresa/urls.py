c0653352ca854498ea23d2bd34c34536aed90193
jorge <jmdinfo123@gmail.com>
2019-07-16 22:33:14 -0300

    teste

--- empresa/urls.py
+++ empresa/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name="empresa"
 
 urlpatterns = [
-    path('/empresa', views.index, name="index"),
+    path('empresa/', views.index, name="index"),
 ]
--- mvpCSA/settings/base.py
+++ mvpCSA/settings/base.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME', 'mvpcsa'),
         'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASS','abc123'),
+        'PASSWORD': os.environ.get('DB_PASS',''),
         'HOST': 'localhost',
         'PORT': '5432',
     }
